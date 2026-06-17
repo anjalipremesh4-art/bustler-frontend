46fbbcf7ea7670897cbfc9b27b3e8559f2dc4fcb
ANJALI P REMESH <anjalipremesh4@gmail.com>
2026-06-17 19:13:43 +0530

    revert: restore ticket_id to 1 after testing

--- src/pages/ResolutionSurvey.jsx
+++ src/pages/ResolutionSurvey.jsx
@@ -24,7 +24,7 @@ function ResolutionSurvey() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          ticket_id: 25,
+          ticket_id: 1,
           user: localStorage.getItem("bustler_user_id"),
           csat_score: rating,
           comment: comment || "No comment",
